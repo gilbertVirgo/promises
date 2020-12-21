ec247d3e503be379125e3a88bfc468873b9892bc
Gil <gilbertjvirgo@gmail.com>
2020-12-21 13:05:36 +0000

    Fixed everything

--- index.js
+++ index.js
@@ -1,33 +1,28 @@
-const {promises: fs, constants: fsConstants} = require("fs");
+const { promises: fs } = require("fs");
 const send = require("./send");
 
+const dayOfYear = () => {
+  const date = new Date();
+  return Math.floor(
+    (date - new Date(date.getFullYear(), 0, 0)) / 1000 / 60 / 60 / 24
+  );
+};
 
 const init = async () => {
-    for(const file of ["promises.json", "profiles.json"]) {
-        await fs.access(file, fsConstants.F_OK).catch(() => {
-            throw new Error("Either promises or profiles are missing.")
-        });
-    }
+  const promises = JSON.parse(await fs.readFile("./promises.json")),
+    profiles = JSON.parse(await fs.readFile("./profiles.json"));
 
-    const promises = JSON.parse(await fs.readFile("./promises.json")),
-        profiles = JSON.parse(await fs.readFile("./profiles.json"));
+  // Fix this line
+  const today = promises[dayOfYear() % promises.length];
 
-    const now = Date.now(),
-        thisYear = new Date((new Date()).getFullYear()),
-        day = 1000 * 60 * 60 * 24;
+  console.log(today);
 
-
-    // Fix this line
-    const today = promises[(now - thisYear) / day];
-       
-    console.log((now - thisYear) / day);
-
-    // for(const {phone} of profiles) {
-    //     await send({
-    //         to: phone,
-    //         body: today
-    //     });
-    // }
-}
+  for (const { phone } of profiles) {
+    await send({
+      to: phone,
+      body: today,
+    });
+  }
+};
 
 init();
